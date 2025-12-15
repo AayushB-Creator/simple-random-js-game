e7b633f4d914488737aebbb38e8d6aa0addbb3f2
Aayush Bharti <aayushb22.cse@gmail.com>
2025-12-15 15:28:49 +0530

    game done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let arr=['csk','mi','rcb','kkr','srh','dc','pbsk','rr','gt','lsg']
+var btn = document.querySelector('button')
+var h2= document.querySelector('h2')
+btn.addEventListener('click',function() {
+var index = Math.floor(    Math.random()*arr.length
+)
+var winner =arr[index]
+console.log(winner);
+h2.innerHTML= winner
+
+})
